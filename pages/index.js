d2f421a39e99f96aa3813de100a7a1abaa5d43ae
Eren <erenglrmns@gmail.com>
2022-06-05 12:02:47 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Subdomain Takeover by F3ap" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          https://hackerone.com/octaviam
         </p>
       </main>
 
